ee0664561edb1fec7e78b9242ea48f0f0855a906
OrangeFrodo <daxoja@gmail.com>
2020-10-22 01:36:02 +0200

    1 "Json add"

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React from 'react';
-import { Logo } from "./components/Logo"
+import { Features } from "./components/Features"
 import { Header } from "./components/Header"
 import './App.css';
 
 function App() {
   return (
-    <Header/>
+    <div>
+      <Header/>
+      <Features />
+    </div>
   );
 }
 
