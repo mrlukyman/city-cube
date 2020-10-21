fbbe9d5cb2e474acde4c673c26199452c5fe6070
Lukas Haring <lukasharing123@gmail.com>
2020-10-22 01:55:34 +0200

    01 branches trying

--- src/components/AppPreview.js
+++ src/components/AppPreview.js
@@ -2,5 +2,10 @@ import React from "react"
 import iPhone from "../assets/iPhone.png"
 
 export const AppPreview = () => (
-    <img syleName="app_preview container" src={iPhone} alt="app-preview"/>
+    <div>
+        <img syleName="app_preview container" src={iPhone} alt="app-preview"/>
+        <p>kokot</p>
+    </div>
+
+
 )
