cb313dc66973ca9a08d7ce5e4b341d3cae1e52b6
Lukas Haring <lukasharing123@gmail.com>
2020-10-22 02:06:54 +0200

    kokot do pice

--- src/components/AppPreview.js
+++ src/components/AppPreview.js
@@ -2,10 +2,5 @@ import React from "react"
 import iPhone from "../assets/iPhone.png"
 
 export const AppPreview = () => (
-    <div>
         <img syleName="app_preview container" src={iPhone} alt="app-preview"/>
-        <p>kokot</p>
-    </div>
-
-
 )
