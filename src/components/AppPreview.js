ce45aefe547a0fe51b10edb81d8d36dd507678a0
Lukas Haring <lukasharing123@gmail.com>
2020-10-22 02:09:58 +0200

    dddd

--- src/components/AppPreview.js
+++ src/components/AppPreview.js
@@ -3,10 +3,4 @@ import iPhone from "../assets/iPhone.png"
 
 export const AppPreview = () => (
         <img syleName="app_preview container" src={iPhone} alt="app-preview"/>
-<<<<<<< HEAD
-=======
-    </div>
-
-
->>>>>>> d6e403cf51d90dbc13fea5410eda3161b3edf410
 )
