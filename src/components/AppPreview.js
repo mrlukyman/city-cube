d6e403cf51d90dbc13fea5410eda3161b3edf410
OrangeFrodo <daxoja@gmail.com>
2020-10-22 02:05:41 +0200

    2 "Kokot delete"

--- src/components/AppPreview.js
+++ src/components/AppPreview.js
@@ -4,7 +4,6 @@ import iPhone from "../assets/iPhone.png"
 export const AppPreview = () => (
     <div>
         <img syleName="app_preview container" src={iPhone} alt="app-preview"/>
-        <p>kokot</p>
     </div>
 
 
