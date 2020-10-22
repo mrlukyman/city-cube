a28820792d54d7a01d2704ff13b54cbf16e2fbc8
OrangeFrodo <daxoja@gmail.com>
2020-10-22 13:13:14 +0200

    5 "Merge fix"

--- src/components/AppPreview.js
+++ src/components/AppPreview.js
@@ -1,11 +1,6 @@
 import React from "react"
 import iPhone from "../assets/iPhone.png"
 
-<<<<<<< HEAD
-export const AppPreview = () => (
-    <img className="app_preview container" src={iPhone} alt="app-preview"/>
-=======
 export const AppPreview = (props) => (
         <img className="app_preview" src={iPhone} alt="app-preview"/>
->>>>>>> origin/feature/initial-changes
 )
