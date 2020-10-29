591aa63596d8ebd37e5cc9862184328bd3514850
OrangeFrodo <daxoja@gmail.com>
2020-10-29 23:58:41 +0100

    Background collor

--- src/components/About.js
+++ src/components/About.js
@@ -3,6 +3,7 @@ import styled from "styled-components"
 import { AboutIcons } from "./AboutIcons"
 import { Container } from "./common/Container"
 import { ContentText } from "./ContentText"
+import { Back } from "./common/Backround"
 import "../index.css"
 
 
@@ -13,17 +14,23 @@ const Wrapper = styled(Container)`
     flex-direction: column;
 `
 
+const WrapperColor = styled(Back)`
+
+`
+
 export const About = (props) => (
-    <Wrapper>
-        <ContentText 
-        size="normal" 
-        headingFontSize="big" 
-        paragraphFontSize="big"
-        hColor="black"
-        hPosition="center"
-        pColor="black"
-        title={props.title} 
-        content={props.content} />
-        <AboutIcons />
-    </Wrapper>
+    <WrapperColor>
+        <Wrapper>
+            <ContentText 
+            size="normal" 
+            headingFontSize="big" 
+            paragraphFontSize="big"
+            hColor="black"
+            hPosition="center"
+            pColor="black"
+            title={props.title} 
+            content={props.content} />
+            <AboutIcons />
+        </Wrapper>
+    </WrapperColor>
 )
---
+++ src/components/common/Backround.js
@@ -0,0 +1,5 @@
+import styled from "styled-components"
+
+export const Back = styled.div`
+    background-color: #F0F0F0;
+`
