eda61265289f74b70035a26ea5a7aed509e28a5d
Lukas Haring <lukasharing123@gmail.com>
2020-11-21 01:45:43 +0100

    Global-components

---
+++ src/components/common/GlobalStyles.js
@@ -0,0 +1,44 @@
+import styled, {createGlobalStyle} from 'styled-components'
+
+export const GlobalStyle = createGlobalStyle`
+    * {
+        box-sizing: border-box;
+        margin: 0;
+        padding: 0;
+        font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto', 'Oxygen',
+        'Ubuntu', 'Cantarell', 'Fira Sans', 'Droid Sans', 'Helvetica Neue',
+        sans-serif;
+        -webkit-font-smoothing: antialiased;
+        -moz-osx-font-smoothing: grayscale;
+        
+    };
+    
+    body {
+        background-color: #262624;
+    };
+    
+    a {
+        text-decoration: none;
+    }
+    `;
+
+// padding-right: 50px;
+//         padding-left: 50px;
+// margin-right: auto;
+//         margin-left: auto;
+
+    export const Container = styled.div`
+        z-index: 1;
+        max-width: 1024px;
+        
+        
+
+        @media screen and (max-width: 991px) {
+            /* padding-right: 30px;
+            padding-left: 30px; */
+        }
+    `;
+
+
+export default GlobalStyle
+
